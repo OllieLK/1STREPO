4cf13775ea3f98ae58f5c4a47bfe4edfaf417031
Oliver Lazarus keene <oli4gaminglk@gmail.com>
2023-07-10 12:18:51 +0100

    Project added plus first code.

---
+++ RepoProject1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+
+namespace RepoProject1
+{
+    class Sprite
+    {
+        public Image i;
+        public Sprite(Uri s)
+        {
+            BitmapImage c = new BitmapImage();
+            c.BeginInit();
+            c.UriSource = s;
+            c.EndInit();
+            i = new Image();
+        }
+    }
+
+    static class GameClass
+    {
+
+    }
+}
---
+++ RepoProject1/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+namespace RepoProject1;
+
+
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        Hide();
+
+
+        InitializeComponent();
+    }
+}
